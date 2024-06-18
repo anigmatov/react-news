ec4ff1eb7f57ed19e66e02b948c05d91254e5d54
Abdusamad Nigmatov <a.nigmatov@newuzbekistanuniversity.uz>
2024-06-18 10:12:57 +0500

    ready

--- react-news/src/App.jsx
+++ react-news/src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
+import NewsBoard from './Components/NewsBoard'
+import NewsItem from './Components/NewsItem'
+import { useState } from 'react'
 
 
 const App = () => {
+  const [category, setCategory] = useState('general');
   return (
     <div>
-      <Navbar />
+      <Navbar setCategory={setCategory}/>
+      <NewsBoard category={category}/>
     </div>
   )
 }
